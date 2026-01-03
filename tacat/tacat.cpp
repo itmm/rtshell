1482ac47bc3654c02cd0366d788ff9169475349b
Timm Knape <timm@knp.de>
2026-01-03 19:43:46 +0100

    std::istream bei tacat verwenden

--- tacat/tacat.cpp
+++ tacat/tacat.cpp
@@ -8,15 +8,13 @@ int main(int argc, const char* argv[]) {
 	try {
 		if (argc != 2) { log_fatal("Syntax", "tacat <archive path>"); }
 		std::string search { argv[1] };
-		ta::Reader reader { std::cin };
-		std::string name;
-		while (reader.open_next_file(name)) {
+		ta::istream in { std::cin };
+		while (auto name = in.open_next_file()) {
 			int ch;
-			if (name == search) {
-				while ((ch = reader.next_ch()) >= 0) { std::cout.put(ch); }
+			if (*name == search) {
+				while ((ch = in.get()) >= 0) { std::cout.put(ch); }
 				return EXIT_SUCCESS;
 			}
-			while ((ch = reader.next_ch()) >= 0) { }
 		}
 	} catch (const terminate_exception&) { }
 	return EXIT_FAILURE;
